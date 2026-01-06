f43623eaa2b0e3c3d842b57807863c4dbf8941bc
Ayrton Marinho <asmarinho@binario.cloud>
2026-01-06 10:14:15 -0300

    hotfix on page counting, if a page is larger than a quantity of X, than it was crashing, now it catches the error, logs and return an empty list

--- api-caller/src/main/java/com/ayrton/api_caller/services/ContactService.java
+++ api-caller/src/main/java/com/ayrton/api_caller/services/ContactService.java
@@ -2,17 +2,22 @@ package com.ayrton.api_caller.services;
 
 import com.ayrton.api_caller.domain.Contact;
 import com.ayrton.api_caller.domain.dto.ContactDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.exc.MismatchedInputException;
 
 import java.net.http.HttpResponse;
+import java.util.Collections;
 import java.util.List;
 
 @Service
+@Slf4j
 public class ContactService {
+
     @Autowired
     private HTTPService httpService;
 
@@ -25,7 +30,13 @@ public class ContactService {
         );
 
         HttpResponse<String>  responseString = httpService.doGetRequest(path);
-        return new ObjectMapper().readValue(responseString.body(), new TypeReference<List<Contact>>() {});
+        try{
+            return new ObjectMapper().readValue(responseString.body(), new TypeReference<List<Contact>>() {});
+        }catch (MismatchedInputException e){
+            log.error(e.getMessage());
+            return Collections.emptyList();
+        }
+
 
     }
 
